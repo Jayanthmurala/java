742810c2ed149628078480012221fe98a351b6aa
Jayanthmurala <165658609+Jayanthmurala@users.noreply.github.com>
2024-10-03 20:19:34 +0530

    Add files via upload

---
+++ oop_abstract/Main.java
@@ -0,0 +1,7 @@
+package oop_abstract;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ oop_abstract/Parent.java
@@ -0,0 +1,6 @@
+package oop_abstract;
+
+public  abstract class Parent {
+    abstract void career();
+    
+}
