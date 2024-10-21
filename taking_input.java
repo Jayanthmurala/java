f1fa9f0aa52d0f9a987513bc1cfee0edaab92653
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2024-10-21 23:50:00 +0530

    Add files via upload

--- taking_input.java
+++ taking_input.java
@@ -1,36 +1,36 @@
-package GeekforGeek;
+// package GeekforGeek;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class taking_input {
+// public class taking_input {
 
-    // Function to take input using Scanner class
-     void IOFunction() {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+//     // Function to take input using Scanner class
+//      void IOFunction() {
+//         Scanner sc = new Scanner(System.in);
+//         int t = sc.nextInt();
 
-        while (t-- > 0) {
-            System.out.println(sc.nextInt());
-            System.out.println(sc.nextDouble());
-            System.out.println(sc.nextLong());
-            System.out.println(sc.nextInt());
-            sc.nextLine();
-            System.out.println(sc.nextLine());
-            sc.close();
+//         while (t-- > 0) {
+//             System.out.println(sc.nextInt());
+//             System.out.println(sc.nextDouble());
+//             System.out.println(sc.nextLong());
+//             System.out.println(sc.nextInt());
+//             sc.nextLine();
+//             System.out.println(sc.nextLine());
+//             sc.close();
 
-        }
-    }
-}
-class GFG {
+//         }
+//     }
+// }
+// class GFG {
 
-    // Driver Code
-    public static void main(String[] args) {
+//     // Driver Code
+//     public static void main(String[] args) {
 
-        // Creating an object of class Geeks
-        taking_input g = new taking_input();
+//         // Creating an object of class Geeks
+//         taking_input g = new taking_input();
 
-        // Calling the IOFunction() of class Geeks
-        g.IOFunction();
-    }
-}
-// } Driver Code Ends
+//         // Calling the IOFunction() of class Geeks
+//         g.IOFunction();
+//     }
+// }
+// // } Driver Code Ends
