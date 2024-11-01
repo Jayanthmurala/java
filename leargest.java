972bc46696f4772bc0db47034db124ee1fc2c857
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2024-11-01 22:15:17 +0530

    Add files via upload

---
+++ leargest.java
@@ -0,0 +1,20 @@
+public class leargest {
+    public static void main(String[] args) {
+        // int a=10541;
+        // int b=10;
+        // int c=100;
+        
+        // if (a>=b && a>=c){
+        //     System.out.println("a is grater");
+            
+        // }else if(b>=a && b>=c) {
+        //     System.out.println("b is grater");
+            
+        // }else{
+        //     System.out.println("c is grater");
+            
+        // }
+        System.out.println(121%10);
+       
+    }
+}
