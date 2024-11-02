b61b03bb7df5a7c986a29fced32bafe933e29d8d
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2024-11-02 23:41:22 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,37 @@
-package Java;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Try obj =  Try.getinc();
-        System.out.println(obj);
-        // Try obj1 =  Try.getinc();
-        // Try obj2 =  Try.getinc();
-        
-        
-        
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter num 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter num 1: ");
+        int b = sc.nextInt();
+        System.out.println("1.+\n2.-\n3.*\n4./\n5.%");
+        System.out.println("Selet from above choose");
+        int ip= sc.nextInt();
+    
+      switch (ip) {
+        case 1:
+          System.out.println(a+b);
+          break;
+        case 2:
+          System.out.println(a-b);
+          break;
+        case 3:
+          System.out.println(a*b);
+          break;
+        case 4:
+          System.out.println(a/b);
+          break;
+        case 5:
+          System.out.println(a%b);
+          break;
+        default:
+          System.out.println("invalied input");
+          break;
+   
+      }
+      sc.close();
     }
-}
+  }
+  
