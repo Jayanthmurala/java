baa3b078b170d611516e5bf7c05848984cc2349a
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2024-11-10 21:17:55 +0530

    Add files via upload

--- UserInput.java
+++ UserInput.java
@@ -1,37 +1,11 @@
-package ex;
-// import java.util.Arrays;
-import java.util.Scanner;
-
-/**
- * UserInput
- */
-class UserInput extends Exception {
-     UserInput(String mesg){
-        super(mesg);
-     }
-}
-/**
- * InnerUserInput
- */
-class InnerUserInput {
-    public static void AgeValidate(int age) throws UserInput{
-        if(age<18){
-            throw new UserInput("not Elagible to vote your below 18");
-        }
-        else{
-            System.out.println("your Elagible to vote");
-        }
+public class UserInput {
+    public static boolean string(int s){
+        String s1 =""+s;
+        StringBuffer N = new StringBuffer();
+        N.append(s);
+        return N.reverse().toString().equals(s1);
     }
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int input=in.nextInt();
-        try{
-            AgeValidate(input);
-        }
-        catch(UserInput e){
-            System.out.println("age: "+input+" "+"is not elagible to vote");
-        }
-        in.close();
+        System.out.println(string(221));
     }
-    
 }
