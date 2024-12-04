bf65ef3ab1fbbcc339c1e9292c4da35e117d684b
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2024-12-04 19:25:19 +0530

    Add files via upload

---
+++ DSA_Java/String Basics/Switch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter num 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter num 1: ");
+        int b = sc.nextInt();
+        System.out.println("1.+\n2.-\n3.*\n4./\n5.%");
+        System.out.println("Selet from above choose");
+        int ip= sc.nextInt();
+    
+      switch (ip) {
+        case 1:
+          System.out.println(a+b);
+          break;
+        case 2:
+          System.out.println(a-b);
+          break;
+        case 3:
+          System.out.println(a*b);
+          break;
+        case 4:
+          System.out.println(a/b);
+          break;
+        case 5:
+          System.out.println(a%b);
+          break;
+        default:
+          System.out.println("invalied input");
+          break;
+   
+      }
+      sc.close();
+    }
+  }
+  
