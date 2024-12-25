84f0d652376f845edfe2c79474f2db9c4fd0474d
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2024-12-25 08:46:13 +0530

    Add files via upload

--- DSA_Java/String Basics/UserInput.java
+++ DSA_Java/String Basics/UserInput.java
@@ -1,11 +1,37 @@
-public class UserInput {
-    public static boolean string(int s){
-        String s1 =""+s;
-        StringBuffer N = new StringBuffer();
-        N.append(s);
-        return N.reverse().toString().equals(s1);
+package ex;
+// import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * UserInput
+ */
+class UserInput extends Exception {
+     UserInput(String mesg){
+        super(mesg);
+     }
+}
+/**
+ * InnerUserInput
+ */
+class InnerUserInput {
+    public static void AgeValidate(int age) throws UserInput{
+        if(age<18){
+            throw new UserInput("not Elagible to vote your below 18");
+        }
+        else{
+            System.out.println("your Elagible to vote");
+        }
     }
     public static void main(String[] args) {
-        System.out.println(string(221));
+        Scanner in = new Scanner(System.in);
+        int input=in.nextInt();
+        try{
+            AgeValidate(input);
+        }
+        catch(UserInput e){
+            System.out.println(e.getMessage());
+        }
+        in.close();
     }
+    
 }
