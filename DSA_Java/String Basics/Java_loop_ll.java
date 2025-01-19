107f5d927b344e7c08d1269b233dc5b26eb4284c
Jayanth Murala <165658609+Jayanthmurala@users.noreply.github.com>
2025-01-19 09:46:24 +0530

    Add files via upload

---
+++ DSA_Java/String Basics/Java_loop_ll.java
@@ -0,0 +1,28 @@
+// package Hackerank;
+// /*https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true */
+
+// import java.util.Scanner;
+// import java.util.*;
+// import java.io.*;
+
+
+// public class Java_loop_ll {
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         // int[] num=new int [6];
+//         int t=in.nextInt();
+//         for(int i = 0;i<t;i++){
+//             int a = in.nextInt();
+//             int b = in.nextInt();
+//             int n = in.nextInt();
+//             int temp=a;
+//             for (int j = 0; j < n; j++) {
+//                 temp+= (int) Math.pow(2, j) * b;
+//                 System.out.print(temp+" ");   
+//             }
+//             System.out.println();   
+ 
+//         }in.close();
+//     }
+// }
+
